3fc50bdedacbc1a1ec1f82e667bc94dd01deb174
Diego Herrera <lainzu79@gmail.com>
2023-03-12 23:04:56 -1100

    funcion encargada de llamar a api

--- js/api.js
+++ js/api.js
@@ -1,5 +1,20 @@
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click" , () =>{
+
+});
+
+const callApi = () =>{
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '337ec89bc4msh66b41e032a1f0f6p166c97jsn86573a36ee9c',
+            'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
     
-})
+    fetch('https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
